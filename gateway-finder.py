d4fe84501be0d6dafa87419cdc0927d12e04eeb9
Pentest Monkey <pentestmonkey@pentestmonkey.net>
2011-08-29 23:07:42 +0100

    More licence info, README update

--- gateway-finder.py
+++ gateway-finder.py
@@ -1,3 +1,27 @@
+# gateway-finder - Tool to identify routers on the local LAN and paths to the Internet
+# Copyright (C) 2011 pentestmonkey@pentestmonkey.net
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as 
+# published by the Free Software Foundation.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# This tool may be used for legal purposes only.  Users take full responsibility
+# for any actions performed using this tool.  If these terms are not acceptable to 
+# you, then do not use this tool.
+# 
+# You are encouraged to send comments, improvements or suggestions to
+# me at pentestmonkey at pentestmonkey.net
+#
+
 from scapy.all import *
 import sys
 import os
